789154083daa4f77ff69550afcb729e67fc5647d
MariaGiron-code <1202mariag@gmail.com>
2025-05-01 09:06:14 -0500

    ACTULIZACION

--- Semana3_POO/main_Cocinero.java
+++ Semana3_POO/main_Cocinero.java
@@ -46,6 +46,28 @@ public class main_Cocinero {
         System.out.println("Los datos del cocinero #3 son: ");
         cocinero3.mostrarInformacion();
 
+        //Reducir las lines de código
+        Cocinero[] cocineros = new Cocinero[3];
+
+        for( int i = 0; i < cocineros.length; i++){
+            //Guardamos los datos en una lista llamada cocineros
+            cocineros[i] = new Cocinero();
+
+            System.out.println("Ingrese los datos del cocinero #" + (i+1) + ":");
+            System.out.println("Ingrese el nombre del cocinero:");
+            cocineros[i].nombre = sc.nextLine();
+
+            System.out.println("Ingrese la especialidad del cocinero:");
+            cocineros[i].especialidad = sc.nextLine();
+
+            System.out.println("Ingrese la experincia del coicero en años: ");
+            cocineros[i].experiencia = sc.nextInt();
+            sc.nextLine();
+
+
+
+        }
+
 
     }
 
